0f60d8f3e12e9a9eb8cce7b087402eeefa170a4e
AbogiC <venturaabedbogichrist314@gmail.com>
2026-04-14 18:04:48 +0700

    update database usable

--- backend/src/database/createDB.sql
+++ backend/src/database/createDB.sql
@@ -32,3 +32,58 @@ CREATE TABLE IF NOT EXISTS sessions (
 -- Insert sample admin user (password: Admin123!)
 INSERT INTO users (name, email, password, role) 
 VALUES ('Admin User', 'admin@example.com', '$2a$12$LQv3c1yqBWVHxkd0LHAkCOYz6TtxMQJqhN8/LewKyYBudgXw3yVU6', 'admin');
+
+---------------------------------------------------------------------------------------------------------------------------------------
+
+-- volunteer table
+CREATE TABLE IF NOT EXISTS volunteer (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    phone VARCHAR(20),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+-- list skills table
+CREATE TABLE IF NOT EXISTS skills (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL UNIQUE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+-- volunteer skills table
+CREATE TABLE IF NOT EXISTS volunteer_skills (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    volunteer_id INT NOT NULL,
+    skill_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (volunteer_id) REFERENCES volunteer(id) ON DELETE CASCADE,
+    FOREIGN KEY (skill_id) REFERENCES skills(id) ON DELETE CASCADE
+);
+
+-- liturgy commission table
+CREATE TABLE IF NOT EXISTS liturgy_commission (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    description TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+-- church service schedule table
+CREATE TABLE IF NOT EXISTS church_service_schedule (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    service_name VARCHAR(100) NOT NULL,
+    service_time TIMESTAMP NOT NULL,
+    service_type ENUM('Sunday Service', 'Wedding', 'Other') NOT NULL,
+    volunteer_id INT NOT NULL,
+    liturgy_commission_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY (liturgy_commission_id) REFERENCES liturgy_commission(id) ON DELETE CASCADE,
+    FOREIGN KEY (volunteer_id) REFERENCES volunteer(id) ON DELETE CASCADE
+);
+
+--------------------------------------------------------------------------------------------------------------------------------------------
